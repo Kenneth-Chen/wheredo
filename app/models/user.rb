a2144c4b41e9454fb9b6b3997f45e59313d570f8
Kenneth Chen <kennchen@gmail.com>
2017-01-29 13:13:56 -0500

    intermediate commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ActiveRecord::Base
+  validates :phone, presence: true
+  validate :phone_format_ok
+  after_validation :standardize_phone
+
+  def standardize_phone
+    self.phone = Phony.normalize(self.phone)
+  end
+
+  def phone_format_ok
+    unless self.phone.present?
+      errors.add :phone, "Please enter a phone number."
+      return false
+    end
+    phone = Phony.format(self.phone)
+    phone = Phony.format("1#{self.phone}") if !Phony.plausible?(self.phone)
+    unless Phony.plausible?(phone)
+      errors.add :phone, "Invalid phone number - please try again."
+      return false
+    end
+    self.phone = phone
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170127034532) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "phone",       limit: 100,                 null: false
     t.string   "email"
