18bc4790af0976220255ec97204af82c87e30af3
Kenneth Chen <kennchen@gmail.com>
2017-02-01 21:11:30 -0800

    try changing mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,9 +78,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.asset_host =  'http://wheredoidrive.com'
+  config.action_mailer.default_url_options = { host: 'http://wheredoidrive.com' }
 
-  config.action_mailer.default_url_options = { host: 'wheredoidrive.com' }
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
